ad6eeca32ce43ce642708d7522a1da7571cf8a2b
Wahyu Oktariansyah <123318464+Whot1966@users.noreply.github.com>
2023-06-11 23:15:50 +0700

    Create plros.sh

---
+++ plros.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/Whot1966/android_bionic-lineage ../../bionic -b lineage-20.0
+git clone https://github.com/Whot1966/android_external_roboto-fonts ../../external/roboto-fonts -b lineage-20.0
+git clone https://github.com/Whot1966/android_frameworks_av-lineage ../../frameworks/av -b lineage-20.0
+git clone https://github.com/Whot1966/android_frameworks_base-lineage ../../frameworks/base -b lineage-20.0
+git clone https://github.com/Whot1966/android_frameworks_libs_systemui-lineage ../../frameworks/libs/systemui -b lineage-20.0
+git clone https://github.com/Whot1966/android_frameworks_native-lineage ../../frameworks/native -b lineage-20.0
+git clone https://github.com/Whot1966/android_hardware_interfaces-lineage ../../hardware/interfaces -b lineage-20.0
+git clone https://github.com/Whot1966/android_lineage-sdk ../../lineage-sdk -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_apps_Backgrounds ../../packages/apps/Backgrounds -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_apps_DeskClock-lineage ../../packages/apps/DeskClock -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_apps_LineageParts ../../packages/apps/LineageParts -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_apps_Nfc-lineage ../../packages/apps/Nfc -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_apps_Settings-lineage ../../packages/apps/Settings -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_apps_ThemePicker-lineage ../../packages/apps/ThemePicker -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_apps_Trebuchet ../../packages/apps/Trebuchet -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_modules_Permission-lineage ../../packages/modules/Permission -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_modules_Permission-lineage ../../packages/overlays/Lineage -b lineage-20.0
+git clone https://github.com/Whot1966/android_packages_overlays_Lineage ../../packages/overlays/Lineage -b lineage-20.0
+git clone https://github.com/Whot1966/android_vendor_lineage ../../vendor/lineage -b lineage-20.0
