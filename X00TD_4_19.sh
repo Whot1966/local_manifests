f0020331f1b0d189111e5c32cb13452f11918d7f
Wahyu Oktariansyah <123318464+Whot1966@users.noreply.github.com>
2023-06-11 23:18:14 +0700

    Update X00TD_4_19.sh

--- X00TD_4_19.sh
+++ X00TD_4_19.sh
@@ -9,6 +9,8 @@ git clone https://github.com/Whot1966/vendor_asus_X00TD-4.19 ../../vendor/asus/X
 rm -rf ../../kernel/asus/
 rm -rf ../../kernel/xiaomi/
 git clone https://github.com/Whot1966/kernel_asus_sdm660-4.19 ../../kernel/asus/sdm660 -b test
+rm -rf ../../system/extras
+git clone https://github.com/LineageOS/android_system_extras ../../system/extras
 rm -rf ../../hardware/qcom-caf/msm8998/audio/
 git clone https://github.com/Whot1966/android_hardware_qcom-caf_msm8998_audio-4.19 ../../hardware/qcom-caf/msm8998/audio/ --depth=1
 rm -rf ../../hardware/qcom-caf/msm8998/display/
